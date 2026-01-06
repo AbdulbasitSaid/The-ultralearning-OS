f4a730ee174a62c549c1b45f70a153ddbe4b5531
AbdulbasitSaid <mrspeed518@gmail.com>
2026-01-06 09:50:53 +0100

    chore: :construction: commit class in progress

--- 2-practical-go/github/github.go
+++ 2-practical-go/github/github.go
@@ -1,3 +1,61 @@
 package main
 
-/// Given a github user login, return name and number of public repos
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// / Given a github user login, return name and number of public repos
+func main() {
+	// resp, err := http.Get("https://api.github.com/users/abdulbasitsaid")
+	// if err != nil {
+	// 	fmt.Println("ERROR:", err)
+	// 	return
+	// }
+	// if resp.StatusCode != http.StatusOK {
+	// 	fmt.Printf("ERROR: bad status - %s\n", resp.Status)
+	// 	return
+	// }
+
+	// ctype := resp.Header.Get("Content-Type")
+	// fmt.Println("content-type:", ctype)
+
+	// var reply struct {
+	// 	Name                string
+	// 	NumberOfPublicRepos int `json:"public_repos"`
+	// }
+
+	// // io.Copy(os.Stdout, resp.Body)
+
+	// dec := json.NewDecoder(resp.Body)
+	// if err := dec.Decode(&reply); err != nil {
+	// 	fmt.Println("ERROR:", err)
+	// 	return
+	// }
+	// fmt.Println(reply.Name, reply.NumberOfPublicRepos)
+	// exercise: try to change the request header.
+}
+
+/// clear example
+
+/// User info return name and number of public repositories
+
+func UserInfo(login string) (name string, repo_count int, err error) {
+	url := "https://api.github.com/users/" + login
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", 0, fmt.Errorf("%q - bad status: %s", url, resp.Status)
+	}
+
+	return parseResponse(resp.Body)
+
+}
+
+func parseResponse(readCloser io.ReadCloser) (string, int, error) {
+	panic("unimplemented")
+}
